19b70ecd695ad3e42429518283a2e189c96180b8
MatteoPadalinoDev <me2llincontact@gmail.com>
2025-05-25 00:55:46 +0200

    add component

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 require("dotenv").config();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // connexion à la DB
 connectDB();
